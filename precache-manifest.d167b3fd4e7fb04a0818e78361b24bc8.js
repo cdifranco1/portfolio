2f564ea671dc87b9d5538fcd87e518cc001163ed
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-08-21 12:48:14 -0500

    Updates

---
+++ precache-manifest.d167b3fd4e7fb04a0818e78361b24bc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114c7f0401f8372152f19ad12d55af69",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4b41fb1333ee7c570f80",
+    "url": "/portfolio/static/css/main.ea1c6b22.chunk.css"
+  },
+  {
+    "revision": "b20af2ae70d15c5ad6e2",
+    "url": "/portfolio/static/js/2.62ce2e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.62ce2e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b41fb1333ee7c570f80",
+    "url": "/portfolio/static/js/main.55800b5a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "de210861951d6da11c7ea22e0c5d79df",
+    "url": "/portfolio/static/media/linkedin.de210861.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
